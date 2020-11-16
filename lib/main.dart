1c4a1ac6559ab84aa364b3a21516ae0ba5dfa27e
Elijah Luckey <luckeyelijah112@gmail.com>
2020-11-16 09:01:53 -0500

    Init commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(WebApp());
+
+class WebApp extends StatelessWidget {
+  const WebApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
